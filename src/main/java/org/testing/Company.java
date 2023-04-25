721c876b9cf825239d18159243c92939d79ba7f7
Patka879 <mysiak.patrycja@gmail.com>
2023-04-25 11:13:36 +0200

    Test case scenarios and functionalities added to Company class

--- src/main/java/org/testing/Company.java
+++ src/main/java/org/testing/Company.java
@@ -1,16 +1,49 @@
 package org.testing;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Company {
-    public static void main(String[] args) {
 
+    private int counter;
+    private int capacity;
+    private String[] employees;
+
+    public Company() {
+        this(1);
     }
+    public Company (int capacity) {
+        this.capacity = capacity;
+        employees = new String[capacity];
+    }
+
     public String[] getEmployee() {
-        return new String[0];
+        return Arrays.copyOf(employees, counter);
     }
     public boolean addEmployee(String name) {
+        if (counter >= capacity) {
+            throw new IllegalArgumentException("The company is full!");
+        }
+
+        employees[counter] = name;
+        counter++;
         return true;
     }
 
+    public boolean removeEmployee(String name) {
+        String[] arr = new String[capacity];
+        int i = 0;
+        boolean success = false;
+        for (String employee : employees) {
+            if (employee == name) {
+                success = true;
+                counter--;
+            } else {
+                arr[i] = employee;
+                i++;
+            }
+        }
+        employees = arr;
+        return success;
+    }
 }
--- src/test/java/CompanyTest.java
+++ src/test/java/CompanyTest.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import org.testing.Company;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CompanyTest {
     @Test
@@ -16,5 +16,54 @@ public class CompanyTest {
         Company company = new Company();
         boolean result = company.addEmployee("Jakub");
         assertTrue(result);
+    }
+
+    @Test
+    public void shouldReturnAddedEmployee() {
+        Company company = new Company(3);
+        company.addEmployee("Jakub");
+        String[] result = company.getEmployee();
+        assertEquals("Jakub",result[0]);
+        assertEquals(1, result.length);
+    }
+
+    @Test
+    public void shouldThrowExceptionIfTooManyEmployeesAreAdded() {
+        Company company = new Company(2);
+        company.addEmployee("Patrycja");
+        company.addEmployee("Jakub");
+
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class,
+                () -> company.addEmployee("Jakub"));
+        assertEquals("The company is full!" , exception.getMessage());
+    }
+
+    @Test
+    public void shouldRemoveEmployeeFromTheList() {
+        Company company = new Company();
+        company.addEmployee("Kuba");
+        boolean result = company.removeEmployee("Kuba");
+        assertEquals(true, result);
+        assertEquals(0, company.getEmployee().length);
+    }
+
+    @Test
+    public void shouldRemoveEmployeeFromTheListAndReturnEmployees() {
+        Company company = new Company(2);
+        company.addEmployee("Kuba");
+        company.addEmployee("Adam");
+        boolean result = company.removeEmployee("Kuba");
+        assertEquals(true, result);
+        assertEquals("Adam", company.getEmployee()[0]);
+    }
+
+    @Test
+    public void shouldNotRemoveEmployeeThatDoesNotExist() {
+        Company company = new Company(2);
+        company.addEmployee("Kuba");
+        company.addEmployee("Adam");
+        boolean result = company.removeEmployee("Kamila");
+        assertEquals(false, result);
+        assertEquals(2, company.getEmployee().length);
     }
 }
